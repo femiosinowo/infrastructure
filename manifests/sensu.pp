c3dc0c9dafee35ee44edb44c2704c2b52677ad77
Femi From Dell <Femi@paosin.com>
2016-04-21 10:34:36 -0400

    added ports to be used

--- manifests/sensu.pp
+++ manifests/sensu.pp
@@ -19,15 +19,48 @@ class infrastructure::sensu () {
     ensure      => present,
     region      => 'us-east-1',
     description => 'sensu security group',
-    ingress     => [{
+    ingress     => [
+      {
         protocol => 'tcp',
-        port     => [3000, 4567, 5672, 8080, 15671, 15672, 4242],
+        port     => '3000',
         cidr     => '0.0.0.0/0',
       }
-      #      ,
-      #      {
-      #        security_group => 'other-security-group',
-      #      }
+      ,
+      {
+        protocol => 'tcp',
+        port     => '4567',
+        cidr     => '0.0.0.0/0',
+      }
+      ,
+      {
+        protocol => 'tcp',
+        port     => '5672',
+        cidr     => '0.0.0.0/0',
+      }
+      ,
+      {
+        protocol => 'tcp',
+        port     => '8080',
+        cidr     => '0.0.0.0/0',
+      }
+      ,
+      {
+        protocol => 'tcp',
+        port     => '15671',
+        cidr     => '0.0.0.0/0',
+      }
+      ,
+      {
+        protocol => 'tcp',
+        port     => '4242',
+        cidr     => '0.0.0.0/0',
+      }
+      ,
+      {
+        protocol => 'tcp',
+        port     => '15672',
+        cidr     => '0.0.0.0/0',
+      }
       ],
     tags        => {
       reason => 'ec2-sensu',
