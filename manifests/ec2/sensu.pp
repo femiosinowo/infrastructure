04d3cd951dc074d65fe6cb53ac492d30f2d2cef5
Femi From Dell <Femi@paosin.com>
2016-04-21 19:28:27 -0400

    removed arn

--- manifests/ec2/sensu.pp
+++ manifests/ec2/sensu.pp
@@ -6,8 +6,7 @@ class infrastructure::ec2::sensu (
   $subnet   = hiera('infrastructure::ec2::subnet'),
   $image_id = hiera('infrastructure::ec2::image_id'),
   $vpc      = hiera('infrastructure::ec2::vpc'),
-  $iam_instance_profile_name = hiera('infrastructure::ec2::iam_instance_profile_name'),
-  $iam_instance_profile_arn = hiera('infrastructure::ec2::iam_instance_profile_arn'),) {
+  $iam_instance_profile_name = hiera('infrastructure::ec2::iam_instance_profile_name'),) {
   ec2_instance { 'sensu-server':
     ensure    => present,
     availability_zone         => $availability_zone,
@@ -19,9 +18,8 @@ class infrastructure::ec2::sensu (
     require   => Ec2_securitygroup['sg_sensu'],
     region    => $region,
     security_groups           => ['sg_sensu'],
-    #iam_instance_profile_name => $iam_instance_profile_name,
+    iam_instance_profile_name => $iam_instance_profile_name,
     subnet    => $subnet,
-    iam_instance_profile_arn => $iam_instance_profile_arn,
     tags      => {
       server_role => 'server_sensu',
     }
