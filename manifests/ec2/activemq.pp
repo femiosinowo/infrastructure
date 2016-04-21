21601224d61336ce2fdb98a92e5725d51bef7bd5
Femi From Dell <Femi@paosin.com>
2016-04-21 14:48:32 -0400

    ensure running for activemq

--- manifests/ec2/activemq.pp
+++ manifests/ec2/activemq.pp
@@ -1,18 +1,14 @@
 class infrastructure::ec2::activemq () {
-ec2_instance { 'activemq':
-  ensure              => 'stopped',
-  availability_zone   => 'us-east-1d',
-  block_devices       => [{'delete_on_termination' => 'true', 'device_name' => '/dev/sda1'}],
-  ebs_optimized       => 'false',
-  hypervisor          => 'xen',
-  image_id            => 'ami-2051294a',
-  instance_id         => 'i-aa4b6d2f',
-  instance_type       => 't2.micro',
-  key_name            => 'DevOps-Keys',
-  monitoring          => 'false',
-  region              => 'us-east-1',
-  security_groups     => ['ActiveMQ'],
-  subnet              => 'DevOps-Public-Subnet',
-  virtualization_type => 'hvm',
-}
+  ec2_instance { 'activemq':
+    ensure            => 'running',
+    availability_zone => 'us-east-1d',
+    ebs_optimized     => 'false',
+    image_id          => 'ami-2051294a',
+    instance_type     => 't2.micro',
+    key_name          => 'DevOps-Keys',
+    monitoring        => 'false',
+    region            => 'us-east-1',
+    security_groups   => ['ActiveMQ'],
+    subnet            => 'DevOps-Public-Subnet',
+  }
 }
