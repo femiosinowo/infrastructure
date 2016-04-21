5caea18a83f5ae2bf9c8d8a838b0870d4da1b7e8
Femi From Dell <Femi@paosin.com>
2016-04-21 18:38:54 -0400

    added iam_instance_profile_name

--- manifests/ec2/sensu.pp
+++ manifests/ec2/sensu.pp
@@ -1,24 +1,26 @@
 class infrastructure::ec2::sensu (
-  $availability_zone = hiera('infrastructure::ec2::availability_zone'),
-  $instance_type     = hiera('infrastructure::ec2::instance_type'),
-  $key_name          = hiera('infrastructure::ec2::key_name'),
-  $region            = hiera('infrastructure::ec2::region'),
-  $subnet            = hiera('infrastructure::ec2::subnet'),
-  $image_id          = hiera('infrastructure::ec2::image_id'),
-  $vpc               = hiera('infrastructure::ec2::vpc'),) {
+  $availability_zone         = hiera('infrastructure::ec2::availability_zone'),
+  $instance_type             = hiera('infrastructure::ec2::instance_type'),
+  $key_name = hiera('infrastructure::ec2::key_name'),
+  $region   = hiera('infrastructure::ec2::region'),
+  $subnet   = hiera('infrastructure::ec2::subnet'),
+  $image_id = hiera('infrastructure::ec2::image_id'),
+  $vpc      = hiera('infrastructure::ec2::vpc'),
+  $iam_instance_profile_name = hiera('infrastructure::ec2::iam_instance_profile_name'),) {
   ec2_instance { 'sensu-server':
-    ensure             => present,
-    availability_zone  => $availability_zone,
-    image_id           => $image_id,
-    instance_type      => $instance_type,
-    key_name           => $key_name,
-    private_ip_address => '10.0.0.50',
-    user_data          => template('infrastructure/userdata.sh.erb'),
-    require            => Ec2_securitygroup['sg_sensu'],
-    region             => $region,
-    security_groups    => ['sg_sensu'],
-    subnet             => $subnet,
-    tags               => {
+    ensure    => present,
+    availability_zone         => $availability_zone,
+    image_id  => $image_id,
+    instance_type             => $instance_type,
+    key_name  => $key_name,
+    private_ip_address        => '10.0.0.50',
+    user_data => template('infrastructure/userdata.sh.erb'),
+    require   => Ec2_securitygroup['sg_sensu'],
+    region    => $region,
+    security_groups           => ['sg_sensu'],
+    iam_instance_profile_name => $iam_instance_profile_name,
+    subnet    => $subnet,
+    tags      => {
       server_role => 'server_sensu',
     }
   }
