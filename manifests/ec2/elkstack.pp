158f3f4a75f09168e4ac21b282dff93cedc8a6de
Femi From Dell <Femi@paosin.com>
2016-04-26 17:03:38 -0400

    present

--- manifests/ec2/elkstack.pp
+++ manifests/ec2/elkstack.pp
@@ -1,7 +1,7 @@
 class infrastructure::ec2::elkstack () {
   infrastructure::ec2::template { 'elkstack':
     hostname                   => 'elkstack.gcio.cloud',
-    ensure_value               => 'absent',
+    ensure_value               => 'present',
     server_role                => 'server_elkstack',
     ip_addr                    => '10.0.0.52',
     security_group_name               => "sg_elkstack",
--- manifests/ec2/template.pp
+++ manifests/ec2/template.pp
@@ -18,66 +18,61 @@ define infrastructure::ec2::template (
   # end of variables
   ) {
   #
-#  if $ensure_value == 'absent' {
-#    exec { "puppet cert clean ${hostname}":
-#      cwd     => "/var/tmp",
-#      path    => ["/usr/bin"],
-#      require => Ec2_instance[$hostname],
-#    }
-#
-#    route53_a_record { "${hostname}.": ensure => $ensure_value, }
-#
-#    ec2_instance { $hostname:
-#      ensure => absent,
-#    }
-#
-#    ec2_securitygroup { $security_group_name:
-#      ensure  => $ensure_value,
-#      require => Ec2_instance[$hostname],
-#    }
-
- # } else {
-    # creating dns record here
-    route53_a_record { "${hostname}.":
-      ensure => $ensure_value,
-      ttl    => '300',
-      values => [$ip_addr],
-      zone   => 'gcio.cloud.',
+  if $ensure_value == 'absent' {
+    exec { "puppet cert clean ${hostname}":
+      cwd     => "/var/tmp",
+      path    => ["/usr/bin"],
+      require => Ec2_instance[$hostname],
     }
 
-    # creating ec2 innstance here
-    ec2_instance { $hostname:
-      ensure    => $ensure_value,
-      availability_zone         => $availability_zone,
-      image_id  => $image_id,
-      instance_type             => $instance_type,
-      key_name  => $key_name,
-      private_ip_address        => $ip_addr,
-      user_data => template('infrastructure/userdata.sh.erb'),
-      require   => Ec2_securitygroup[$security_group_name],
-      region    => $region,
-      security_groups           => [$security_group_name],
-      iam_instance_profile_name => $iam_instance_profile_name,
-      subnet    => $subnet,
-      tags      => {
-        server_role => $server_role,
-      }
-    }
-
-    # creating security groups here
     ec2_securitygroup { $security_group_name:
-      #ensure      => $ensure_value,
-      region      => $region,
-      vpc         => $vpc,
-      description => $security_group_description,
-      ingress     => $security_group_ingress,
-      #require     => Ec2_instance[$hostname],
-
-    #      tags        => {
-    #        reason => $security_group_name,
-    #      }
-    #      ,
+      ensure  => $ensure_value,
+      require => Ec2_instance[$hostname],
     }
+  }
+
+  # else {
+  # creating dns record here
+  route53_a_record { "${hostname}.":
+    ensure => $ensure_value,
+    ttl    => '300',
+    values => [$ip_addr],
+    zone   => 'gcio.cloud.',
+  }
+
+  # creating ec2 innstance here
+  ec2_instance { $hostname:
+    ensure    => $ensure_value,
+    availability_zone         => $availability_zone,
+    image_id  => $image_id,
+    instance_type             => $instance_type,
+    key_name  => $key_name,
+    private_ip_address        => $ip_addr,
+    user_data => template('infrastructure/userdata.sh.erb'),
+    require   => Ec2_securitygroup[$security_group_name],
+    region    => $region,
+    security_groups           => [$security_group_name],
+    iam_instance_profile_name => $iam_instance_profile_name,
+    subnet    => $subnet,
+    tags      => {
+      server_role => $server_role,
+    }
+  }
+
+  # creating security groups here
+  ec2_securitygroup { $security_group_name:
+    # ensure      => $ensure_value,
+    region      => $region,
+    vpc         => $vpc,
+    description => $security_group_description,
+    ingress     => $security_group_ingress,
+  # require     => Ec2_instance[$hostname],
+
+  #      tags        => {
+  #        reason => $security_group_name,
+  #      }
+  #      ,
+  }
 
   #}
 }
