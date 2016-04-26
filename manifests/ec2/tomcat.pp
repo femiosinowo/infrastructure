fc5917deb817c2e0495870148e333c75f4106716
femio <femio@DESKTOP-7T6QEFN>
2016-04-26 00:00:52 -0400

    added if statuement

--- manifests/ec2/tomcat.pp
+++ manifests/ec2/tomcat.pp
@@ -1,5 +1,6 @@
 class infrastructure::ec2::tomcat (
-  $hostname = 'tomcat.gcio.cloud.', # don't forget it must always end with a dot.
+  $hostname = 'tomcat.gcio.cloud.',
+  # don't forget it must always end with a dot.
   $ensure_value              = 'absent',
   $server_role               = 'server_tomcat',
   $ip_addr  = '10.0.0.51',
@@ -18,61 +19,66 @@ class infrastructure::ec2::tomcat (
       cwd  => "/var/tmp",
       path => ["/usr/bin"]
     }
-  }
 
-  route53_a_record { $hostname:
-    ensure => $ensure_value,
-    ttl    => '300',
-    values => [$ip_addr],
-    zone   => 'gcio.cloud.',
-  }
+    route53_a_record { $hostname: ensure => $ensure_value, }
 
-  ec2_instance { $server_role:
-    ensure    => $ensure_value,
-    availability_zone         => $availability_zone,
-    image_id  => $image_id,
-    instance_type             => $instance_type,
-    key_name  => $key_name,
-    private_ip_address        => $ip_addr,
-    user_data => template('infrastructure/userdata.sh.erb'),
-    require   => Ec2_securitygroup[$security_group_name],
-    region    => $region,
-    security_groups           => [$security_group_name],
-    iam_instance_profile_name => $iam_instance_profile_name,
-    subnet    => $subnet,
-    tags      => {
-      server_role => 'server_tomcat',
+    ec2_instance { $server_role: ensure => $ensure_value, }
+
+  } else {
+    route53_a_record { $hostname:
+      ensure => $ensure_value,
+      ttl    => '300',
+      values => [$ip_addr],
+      zone   => 'gcio.cloud.',
     }
-  }
 
-  #  fact { 'server_role':
-  #    content => 'server_sensu',
-  #    ensure  => present,
-  #  }
+    ec2_instance { $server_role:
+      ensure    => $ensure_value,
+      availability_zone         => $availability_zone,
+      image_id  => $image_id,
+      instance_type             => $instance_type,
+      key_name  => $key_name,
+      private_ip_address        => $ip_addr,
+      user_data => template('infrastructure/userdata.sh.erb'),
+      require   => Ec2_securitygroup[$security_group_name],
+      region    => $region,
+      security_groups           => [$security_group_name],
+      iam_instance_profile_name => $iam_instance_profile_name,
+      subnet    => $subnet,
+      tags      => {
+        server_role => 'server_tomcat',
+      }
+    }
 
-  ec2_securitygroup { $security_group_name:
-    ensure      => $ensure_value,
-    region      => $region,
-    vpc         => $vpc,
-    description => 'Tomcat Security group',
-    # require     => Ec2_instance[$server_role],
-    ingress     => [
-      {
-        protocol => 'tcp',
-        port     => '22',
-        cidr     => '0.0.0.0/0',
+    #  fact { 'server_role':
+    #    content => 'server_sensu',
+    #    ensure  => present,
+    #  }
+
+    ec2_securitygroup { $security_group_name:
+      ensure      => $ensure_value,
+      region      => $region,
+      vpc         => $vpc,
+      description => 'Tomcat Security group',
+      # require     => Ec2_instance[$server_role],
+      ingress     => [
+        {
+          protocol => 'tcp',
+          port     => '22',
+          cidr     => '0.0.0.0/0',
+        }
+        ,
+        {
+          protocol => 'tcp',
+          port     => '8080',
+          cidr     => '0.0.0.0/0',
+        }
+
+        ],
+      tags        => {
+        reason => $security_group_name,
       }
       ,
-      {
-        protocol => 'tcp',
-        port     => '8080',
-        cidr     => '0.0.0.0/0',
-      }
-
-      ],
-    tags        => {
-      reason => $security_group_name,
     }
-    ,
   }
 }
